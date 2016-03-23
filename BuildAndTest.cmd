21967590187e811b67b5d7698f8ca831be0088fc
Nick Guerrera <nicholg@microsoft.com>
2016-03-23 15:18:41 -0700

    Update corefx dependency from submodule to NuGet.config Make BuildAndTest.cmd clone submodules and restore roslyn nuget packages

--- BuildAndTest.cmd
+++ BuildAndTest.cmd
@@ -12,10 +12,11 @@ set PRERELEASE=-beta
 
 set VERSION_CONSTANTS=src\BinaryParsers\VersionConstants.cs
 
-rd /s /q bld
+if exist bld ( rd /s /q bld || exit /b 1 )
 
 @REM Rewrite VersionConstants.cs
-echo // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT        > %VERSION_CONSTANTS%
+echo Rewriting VersionConstants.cs...
+echo // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT       >  %VERSION_CONSTANTS% 
 echo // license. See LICENSE file in the project root for full license information.>> %VERSION_CONSTANTS%
 echo namespace Microsoft.CodeAnalysis.IL                                           >> %VERSION_CONSTANTS%
 echo {                                                                             >> %VERSION_CONSTANTS%
@@ -28,13 +29,24 @@ echo         public const string Version = AssemblyVersion + Prerelease;        
 echo     }                                                                         >> %VERSION_CONSTANTS%
 echo  }                                                                            >> %VERSION_CONSTANTS%
 
+echo Updating submodules...
+git submodule update --init --recursive || exit /b 1
 
-%~dp0.nuget\NuGet.exe restore src\BinSkim.sln 
-msbuild /verbosity:minimal /target:rebuild src\BinSkim.sln /p:Configuration=Release /p:"Platform=x64"
-msbuild /verbosity:minimal /target:rebuild src\BinSkim.sln /p:Configuration=Release /p:"Platform=x86"
+echo Restoring roslyn nuget dependencies...
+call src\Roslyn\Restore.cmd || exit /b 1
 
+echo Restoring binskim nuget dependencies...
+%~dp0.nuget\NuGet.exe restore src\BinSkim.sln || exit /b 1
+
+echo Building x64...
+msbuild /verbosity:minimal /target:rebuild src\BinSkim.sln /p:Configuration=Release /p:"Platform=x64" || exit /b 1
+
+echo Building x86...
+msbuild /verbosity:minimal /target:rebuild src\BinSkim.sln /p:Configuration=Release /p:"Platform=x86" || exit /b 1
+
+echo Making nuget package...
 md bld\bin\nuget
+.nuget\NuGet.exe pack .\src\Nuget\BinSkim.nuspec -Symbols -Properties id=Microsoft.CodeAnalysis.BinSkim;major=%MAJOR%;minor=%MINOR%;patch=%PATCH%;prerelease=%PRERELEASE% -Verbosity Quiet -BasePath .\bld\bin\BinSkim.Driver -OutputDirectory .\bld\bin\Nuget || exit /b 1
 
-.nuget\NuGet.exe pack .\src\Nuget\BinSkim.nuspec -Symbols -Properties id=Microsoft.CodeAnalysis.BinSkim;major=%MAJOR%;minor=%MINOR%;patch=%PATCH%;prerelease=%PRERELEASE% -Verbosity Quiet -BasePath .\bld\bin\BinSkim.Driver -OutputDirectory .\bld\bin\Nuget
-
+echo Running unit tests...
 src\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe bld\bin\BinSkim.Rules.FunctionalTests\x86_Release\BinSkim.Rules.FunctionalTests.dll
