df32eee9286fa054705d96044ea91eb8a7fa603a
Chris Meyer <v-chrmey@microsoft.com>
2018-01-09 14:32:30 -0800

    BuildPackages update

--- BuildPackages.cmd
+++ BuildPackages.cmd
@@ -4,7 +4,7 @@ SETLOCAL
 
 call SetCurrentVersion.cmd
 
-.nuget\NuGet.exe pack .\src\Nuget\BinSkim.nuspec -Symbols -Properties major=%MAJOR%;minor=%MINOR%;patch=%PATCH%;prerelease=%PRERELEASE% -Verbosity Quiet -BasePath .\bld\bin -OutputDirectory .\bld\bin\Nuget || goto :ExitFailed
+.nuget\NuGet.exe pack .\src\Nuget\BinSkim.nuspec -Symbols -Properties configuration=%Configuration%;version=%MAJOR%.%MINOR%.%PATCH%%PRERELEASE% -Verbosity Quiet -BasePath .\ -OutputDirectory .\bld\bin\Nuget || goto :ExitFailed
 
 goto Exit
 
