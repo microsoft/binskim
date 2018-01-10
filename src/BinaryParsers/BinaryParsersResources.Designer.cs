20d85f901ea88fa00654ec17f977d7b7d0fda874
Everett Maus <evmaus@microsoft.com>
2018-01-09 17:18:10 -0800

    Target framework change--using netstandard for libraries.  Small fixes to BinaryParsers, test fixes to ILDiagnosticsAnalyzer.

--- src/BinaryParsers/BinaryParsersResources.Designer.cs
+++ src/BinaryParsers/BinaryParsersResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.CodeAnalysis.BinaryData {
+namespace Microsoft.CodeAnalysis.BinaryParsers {
     using System;
     
     
