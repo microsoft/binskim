584c254e8cae413177b941cc2caef5e3dc2e53bb
Everett Maus <evmaus@microsoft.com>
2018-07-18 15:13:32 -0700

    Fixing bad import
    Local machine had a non-shipping copy of the SARIF driver cached.

--- src/BinSkim.Driver/BinSkim.cs
+++ src/BinSkim.Driver/BinSkim.cs
@@ -10,7 +10,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.CodeAnalysis.Sarif;
-using Microsoft.CodeAnalysis.Sarif.Driver.Sdk;
 
 namespace Microsoft.CodeAnalysis.IL
 {
