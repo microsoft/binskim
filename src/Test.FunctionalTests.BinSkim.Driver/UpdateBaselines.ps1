3bbfec5ae12acaf0efc293f626b5db5257e24226
Michael C. Fanning <mikefan@microsoft.com>
2019-09-12 13:13:58 -0700

    Update all tests to final SARIF 2.1.  Add dotnet core test binaries (for entrypoint DLL).

--- src/Test.FunctionalTests.BinSkim.Driver/UpdateBaselines.ps1
+++ src/Test.FunctionalTests.BinSkim.Driver/UpdateBaselines.ps1
@@ -33,8 +33,8 @@ function Build-Baselines($sourceExtension)
 
         # Actually run the tool
         Remove-Item $outputTemp -ErrorAction SilentlyContinue
-        Write-Host "$utility analyze "$input" --output "$outputTemp" --pretty-print --verbose --hashes --config default --quiet"
-        &           $utility analyze "$input" --output "$outputTemp" --pretty-print --verbose --hashes --config default --quiet
+        Write-Host "$utility analyze "$input" --output "$outputTemp" --pretty-print --verbose --hashes --config default --quiet --sarif-output-version Current"
+        &           $utility analyze "$input" --output "$outputTemp" --pretty-print --verbose --hashes --config default --quiet --sarif-output-version Current
 
         # Replace repository root absolute path with Z:\ for machine and enlistment independence
         $text = [IO.File]::ReadAllText($outputTemp)
