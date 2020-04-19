635fe0043deb0178846fa536a783a2c639dbfb38
Michael C. Fanning <michael.fanning@microsoft.com>
2020-04-19 12:44:21 -0700

    Update to dotnet 3.1. (#271)
    * Update to netcoreapp3.1.
    
    * Update release notes for .NET Core 3.1 change. Ignore *.deb files

--- CreatePackagesFromLayoutDirectory.cmd
+++ CreatePackagesFromLayoutDirectory.cmd
@@ -36,19 +36,19 @@ call BuildPackages.cmd %Configuration% %Platform% %NuGetOutputDirectory% %Versio
 goto :Exit
 
 :CopyExeForSigning
-xcopy /Y %LayoutForSigningDirectory%\netcoreapp2.0\win-x86\%~n1.exe %BinaryOutputDirectory%\netcoreapp2.0\win-x86\ 
+xcopy /Y %LayoutForSigningDirectory%\netcoreapp3.1\win-x86\%~n1.exe %BinaryOutputDirectory%\netcoreapp3.1\win-x86\ 
 if "%ERRORLEVEL%" NEQ "0" (echo %1 assembly copy failed. && goto :ExeFilesExit)
-xcopy /Y %LayoutForSigningDirectory%\netcoreapp2.0\win-x64\%~n1.exe %BinaryOutputDirectory%\netcoreapp2.0\win-x64\ 
+xcopy /Y %LayoutForSigningDirectory%\netcoreapp3.1\win-x64\%~n1.exe %BinaryOutputDirectory%\netcoreapp3.1\win-x64\ 
 if "%ERRORLEVEL%" NEQ "0" (echo %1 assembly copy failed. && goto :ExeFilesExit)
 :ExeFilesExit
 Exit /B %ERRORLEVEL%
 
 :CopyFilesForMultitargeting
-xcopy /Y %BinaryOutputDirectory%\netcoreapp2.0\win-x86\%~n1.dll %LayoutForSigningDirectory%\netcoreapp2.0\win-x86\ 
+xcopy /Y %BinaryOutputDirectory%\netcoreapp3.1\win-x86\%~n1.dll %LayoutForSigningDirectory%\netcoreapp3.1\win-x86\ 
 if "%ERRORLEVEL%" NEQ "0" (echo %1 assembly copy failed. && goto :CopyFilesExit)
-xcopy /Y %BinaryOutputDirectory%\netcoreapp2.0\win-x64\%~n1.dll %LayoutForSigningDirectory%\netcoreapp2.0\win-x64\ 
+xcopy /Y %BinaryOutputDirectory%\netcoreapp3.1\win-x64\%~n1.dll %LayoutForSigningDirectory%\netcoreapp3.1\win-x64\ 
 if "%ERRORLEVEL%" NEQ "0" (echo %1 assembly copy failed. && goto :CopyFilesExit)
-xcopy /Y %BinaryOutputDirectory%\netcoreapp2.0\linux-x64\%~n1.dll  %LayoutForSigningDirectory%\netcoreapp2.0\linux-x64\
+xcopy /Y %BinaryOutputDirectory%\netcoreapp3.1\linux-x64\%~n1.dll  %LayoutForSigningDirectory%\netcoreapp3.1\linux-x64\
 if "%ERRORLEVEL%" NEQ "0" (echo %1 assembly copy failed. && goto :CopyFilesExit)
 :CopyFilesExit
 Exit /B %ERRORLEVEL%
