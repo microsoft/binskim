9f4679e2d5fc2f0c5684baa8de4836ea6927d7ab
Eddy Nakamura <eddynaka@gmail.com>
2020-12-16 18:54:43 -0300

    Upgrading packages (#315)

--- src/Test.UnitTests.BinaryParsers/PEBinary/PEBinaryTests.cs
+++ src/Test.UnitTests.BinaryParsers/PEBinary/PEBinaryTests.cs
@@ -75,7 +75,7 @@ namespace Microsoft.CodeAnalysis.BinaryParsers
             if (!PlatformSpecificHelpers.RunningOnWindows()) { return; }
 
             Action action = () => { IDiaDataSource source = ProgramDatabase.MsdiaComWrapper.GetDiaSource(); };
-            action.ShouldNotThrow();
+            action.Should().NotThrow();
         }
     }
 }
