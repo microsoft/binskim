3bc7aa9264d0656314098b80a6340fa91cb89565
Michael Fanning <mikefan@microsoft.com>
2021-02-22 15:21:03 -0800

    Update baselines.

--- src/BinSkim.Rules/PERules/BA2006.BuildWithSecureTools.cs
+++ src/BinSkim.Rules/PERules/BA2006.BuildWithSecureTools.cs
@@ -334,7 +334,7 @@ namespace Microsoft.CodeAnalysis.IL.Rules
                 [nameof(Language.MASM)] = new Version(14, 15, 26706, 0),
                 [nameof(Language.LINK)] = new Version(17, 0, 65501, 17013),
                 [nameof(Language.CSharp)] = new Version(19, 0, 0, 0),
-                [nameof(Language.CVTRES)] = new Version(14, 14, 26430, 0),
+                [nameof(Language.CVTRES)] = new Version(12, 0, 0, 0),
                 [nameof(Language.Unknown)] = new Version(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue),
                 [MIN_XBOX_COMPILER_VER] = new Version(16, 0, 11886, 0)
             };
