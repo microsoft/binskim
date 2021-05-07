682da00747734fa62ff65e972b1ec188d8e76d19
Michael Fanning <mikefan@microsoft.com>
2021-05-07 09:09:41 -0700

    Fix null deref issue.

--- src/BinSkim.Rules/PERules/BA2006.BuildWithSecureTools.cs
+++ src/BinSkim.Rules/PERules/BA2006.BuildWithSecureTools.cs
@@ -206,6 +206,7 @@ namespace Microsoft.CodeAnalysis.IL.Rules
 
                 AdvancedMitigations advancedMitigations = context.Policy.GetProperty(AdvancedMitigationsEnforced);
                 if (!foundIssue &&
+                    target.PE != null &&
                     (advancedMitigations & AdvancedMitigations.Spectre) == AdvancedMitigations.Spectre)
                 {
                     var machineType = (ExtendedMachine)target.PE.Machine;
