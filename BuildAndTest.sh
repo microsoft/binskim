f5b5c6a3b6a78a4ec0cd3ad302b79c6114aa5762
Eddy Nakamura <ednakamu@microsoft.com>
2021-07-05 20:57:50 -0300

    Updating sh script

--- BuildAndTest.sh
+++ BuildAndTest.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-dotnet build src/*.sln --configuration Release
-dotnet test src/*.sln --configuration Release --no-build
+if [[  "$(uname)" == "Linux" ]]; then
+  echo "Changing paths in BinSkim SLN to non-Windows paths due to msbuild issue #1957 (https://github.com/microsoft/msbuild/issues/1957)"
+  sed 's#\\#/#g' src/BinSkim.sln > src/BinSkimLinux.sln
+fi
+
+dotnet build src/BinSkimLinux.sln --configuration Release
+
+dotnet test src/Test.FunctionalTests.BinSkim.Driver/Test.FunctionalTests.BinSkim.Driver.csproj --no-build --configuration Release
+dotnet test src/Test.FunctionalTests.BinSkim.Rules/Test.FunctionalTests.BinSkim.Rules.csproj --no-build --configuration Release
+dotnet test src/Test.UnitTests.BinaryParsers/Test.UnitTests.BinaryParsers.csproj --no-build --configuration Release
+dotnet test src/Test.UnitTests.BinSkim.Rules/Test.UnitTests.BinSkim.Rules.csproj --no-build --configuration Release
