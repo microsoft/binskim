ca47cb2be71a80a45c3b04e29d31b5c9395c24c9
Shaopeng <81775155+shaopeng-gh@users.noreply.github.com>
2021-07-13 19:34:09 -0700

    ELF support debug link

---
+++ src/BinaryParsers/ElfBinary/Elf/SectionName.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.CodeAnalysis.BinaryParsers.Elf
+{
+    public static class SectionName
+    {
+        public const string Data = ".data";
+        public const string DebugAbbrev = ".debug_abbrev";
+        public const string DebugFrame = ".debug_frame";
+        public const string DebugInfo = ".debug_info";
+        public const string DebugInfoDwo = ".debug_info.dwo";
+        public const string DebugLine = ".debug_line";
+        public const string DebugStr = ".debug_str";
+        public const string Dynsym = ".dynsym";
+        public const string EhFrame = ".eh_frame";
+        public const string GnuDebugLink = ".gnu_debuglink";
+        public const string Init = ".init";
+        public const string Interp = ".interp";
+        public const string Text = ".text";
+    }
+}
