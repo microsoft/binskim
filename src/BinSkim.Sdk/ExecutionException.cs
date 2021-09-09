1ee5f41705943cedb7d533c14a8580e9e2cd64fc
Yong Yan <yanyong-gh@outlook.com>
2021-09-09 12:50:09 -0700

    Send exceptions data to AppInsight (#477)

---
+++ src/BinSkim.Sdk/ExecutionException.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.IL.Sdk
+{
+    public class ExecutionException : Exception
+    {
+        private readonly string stackTrace;
+
+        private readonly string originalType;
+
+        public ExecutionException(string type, string message, string stackTrace, Exception innerException)
+            : base(message, innerException)
+        {
+            this.originalType = type;
+            this.stackTrace = stackTrace;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.originalType},{this.Message},{this.StackTrace}";
+        }
+
+        public string OriginalType => this.originalType;
+
+        public override string StackTrace => this.stackTrace;
+    }
+}
