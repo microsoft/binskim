cb38931d1782ca82cf40b1d247a1c9302ca85b52
Michael C. Fanning <mikefan@microsoft.com>
2022-01-26 09:23:56 -0800

    Add renamed files.

---
+++ src/BinSkim.Driver/MultithreadedAnalyzeCommand.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis.IL.Rules;
+using Microsoft.CodeAnalysis.IL.Sdk;
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.Driver;
+using Microsoft.CodeAnalysis.Sarif.Readers;
+using Microsoft.CodeAnalysis.Sarif.VersionOne;
+using Microsoft.CodeAnalysis.Sarif.Visitors;
+
+namespace Microsoft.CodeAnalysis.IL
+{
+    public class MultithreadedAnalyzeCommand : MultithreadedAnalyzeCommandBase<BinaryAnalyzerContext, AnalyzeOptions>
+    {
+        private static bool ShouldWarnVerbose = true;
+
+        public static HashSet<string> ValidAnalysisFileExtensions = new HashSet<string>(
+            new string[] { ".dll", ".exe", ".sys" }
+            );
+
+        public override IEnumerable<Assembly> DefaultPluginAssemblies
+        {
+            get => new Assembly[] { typeof(MarkImageAsNXCompatible).Assembly };
+            set => throw new InvalidOperationException();
+        }
+
+        protected override BinaryAnalyzerContext CreateContext(AnalyzeOptions options, IAnalysisLogger logger, RuntimeConditions runtimeErrors, PropertiesDictionary policy = null, string filePath = null)
+        {
+            BinaryAnalyzerContext binaryAnalyzerContext = base.CreateContext(options, logger, runtimeErrors, policy, filePath);
+
+            // Update context object based on command-line parameters.
+            binaryAnalyzerContext.ForceOverwrite = options.Force;
+            binaryAnalyzerContext.SymbolPath = options.SymbolsPath;
+            binaryAnalyzerContext.IgnorePdbLoadError = options.IgnorePdbLoadError;
+            binaryAnalyzerContext.LocalSymbolDirectories = options.LocalSymbolDirectories;
+            binaryAnalyzerContext.TracePdbLoads = options.Traces.Contains(nameof(Traces.PdbLoad));
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            if (options.Verbose && ShouldWarnVerbose)
+#pragma warning restore CS0618 // Type or member is obsolete
+            {
+                Warnings.LogObsoleteOption(binaryAnalyzerContext, "--verbose", Sdk.SdkResources.Verbose_ReplaceWithLevelAndKind);
+                ShouldWarnVerbose = false;
+            }
+
+            return binaryAnalyzerContext;
+        }
+
+        protected override void InitializeConfiguration(AnalyzeOptions options, BinaryAnalyzerContext context)
+        {
+            base.InitializeConfiguration(options, context);
+
+            // Command-line provided policy is now initialized. Update context 
+            // based on any possible configuration provided in this way.
+
+            context.CompilerDataLogger = new CompilerDataLogger(context);
+        }
+
+        public override int Run(AnalyzeOptions analyzeOptions)
+        {
+            if (!Environment.GetCommandLineArgs().Any(arg => arg.Equals("--sarif-output-version")))
+            {
+                analyzeOptions.SarifOutputVersion = Sarif.SarifVersion.Current;
+            }
+
+            if (s_UnitTestOutputVersion != Sarif.SarifVersion.Unknown)
+            {
+                analyzeOptions.SarifOutputVersion = s_UnitTestOutputVersion;
+            }
+
+// Type or member is obsolete
+#pragma warning disable CS0618
+            if (analyzeOptions.Verbose)
+#pragma warning restore CS0618
+            {
+                analyzeOptions.Kind = new List<ResultKind> { ResultKind.Fail, ResultKind.NotApplicable, ResultKind.Pass };
+                analyzeOptions.Level = new List<FailureLevel> { FailureLevel.Error, FailureLevel.Warning, FailureLevel.Note };
+            }
+
+// Type or member is obsolete
+#pragma warning disable CS0618 
+            if (analyzeOptions.ComputeFileHashes)
+#pragma warning restore CS0618
+            {
+                OptionallyEmittedData dataToInsert = analyzeOptions.DataToInsert.ToFlags();
+                dataToInsert |= OptionallyEmittedData.Hashes;
+
+                analyzeOptions.DataToInsert = Enum.GetValues(typeof(OptionallyEmittedData)).Cast<OptionallyEmittedData>()
+                    .Where(oed => dataToInsert.HasFlag(oed)).ToList();
+            }
+
+            int result = 0;
+
+            try
+            {
+                result = base.Run(analyzeOptions);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            /*
+            try
+            {
+                if (CompilerDataLogger.TelemetryEnabled &&
+                    !string.IsNullOrEmpty(analyzeOptions.OutputFilePath) &&
+                    this.FileSystem.FileExists(analyzeOptions.OutputFilePath))
+                {
+                    SarifLog sarifLog = ReadSarifLog(this.FileSystem, analyzeOptions);
+
+                    AnalysisSummary summary = AnalysisSummaryExtractor.ExtractAnalysisSummary(
+                        sarifLog, analyzeOptions);
+                    CompilerDataLogger.Summarize(summary);
+
+                    IEnumerable<ExecutionException> exceptions = AnalysisSummaryExtractor.ExtractExceptionData(sarifLog);
+                    foreach (ExecutionException ex in exceptions)
+                    {
+                        CompilerDataLogger.WriteException(ex, summary);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                CompilerDataLogger.Flush();
+            }
+            */
+
+            // In BinSkim, no rule is ever applicable to every target type. For example,
+            // we have checks that are only relevant to either 32-bit or 64-bit binaries.
+            // Because of this, the return code bit for RuleNotApplicableToTarget is not
+            // interesting (it will always be set).
+
+            return analyzeOptions.RichReturnCode
+                ? (int)((uint)result & ~(uint)RuntimeConditions.RuleNotApplicableToTarget)
+                : result;
+        }
+
+        internal static SarifLog ReadSarifLog(IFileSystem fileSystem, AnalyzeOptions analyzeOptions)
+        {
+            SarifLog sarifLog;
+            if (analyzeOptions.SarifOutputVersion == Sarif.SarifVersion.Current)
+            {
+                sarifLog = SarifLog.Load(analyzeOptions.OutputFilePath);
+            }
+            else
+            {
+                SarifLogVersionOne actualLog = ReadSarifFile<SarifLogVersionOne>(fileSystem, analyzeOptions.OutputFilePath, SarifContractResolverVersionOne.Instance);
+                var visitor = new SarifVersionOneToCurrentVisitor();
+                visitor.VisitSarifLogVersionOne(actualLog);
+                sarifLog = visitor.SarifLog;
+            }
+
+            return sarifLog;
+        }
+
+        internal static Sarif.SarifVersion s_UnitTestOutputVersion;
+    }
+}
---
+++ src/BinSkim.Rules/PERules/BA4001.ReportPortableExecutableCompilerData.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+
+using Microsoft.CodeAnalysis.BinaryParsers;
+using Microsoft.CodeAnalysis.BinaryParsers.ProgramDatabase;
+using Microsoft.CodeAnalysis.IL.Sdk;
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.Driver;
+
+namespace Microsoft.CodeAnalysis.IL.Rules
+{
+    [Export(typeof(Skimmer<BinaryAnalyzerContext>)), Export(typeof(ReportingDescriptor)), Export(typeof(IOptionsProvider))]
+    public class ReportPECompilerData : WindowsBinaryAndPdbSkimmerBase, IOptionsProvider
+    {
+        /// <summary>
+        /// BA4001
+        /// </summary>
+        public override string Id => RuleIds.ReportPortableExecutableCompilerData;
+
+        public override bool LogPdbLoadException => false;
+
+        /// <summary>
+        /// This rule emits CSV data to the console for every compiler/language/version
+        /// combination that's observed in any PDB-linked compiland.
+        /// </summary>
+        public override MultiformatMessageString FullDescription => new MultiformatMessageString { Text = RuleResources.BA4001_ReportPECompilerData_Description };
+
+        protected override IEnumerable<string> MessageResourceNames => Array.Empty<string>();
+
+        public IEnumerable<IOption> GetOptions()
+        {
+            return new List<IOption>
+            {
+                CompilerDataLogger.CsvOutputPath,
+            }.ToImmutableArray();
+        }
+
+        public override AnalysisApplicability CanAnalyzePE(PEBinary target, PropertiesDictionary policy, out string reasonForNotAnalyzing)
+        {
+            reasonForNotAnalyzing = null;
+            return AnalysisApplicability.ApplicableToSpecifiedTarget;
+        }
+
+        public override void AnalyzePortableExecutableAndPdb(BinaryAnalyzerContext context)
+        {
+            if (!context.CompilerDataLogger.Enabled)
+            {
+                return;
+            }
+
+            PEBinary target = context.PEBinary();
+            Pdb pdb = target.Pdb;
+
+            if (pdb == null)
+            {
+                string errorMessage = target.PdbParseException.Message;
+                context.CompilerDataLogger.WriteException(context, errorMessage);
+                return;
+            }
+
+            var records = new Dictionary<CompilerData, ObjectModuleDetails>();
+
+            if (target.PE.IsManaged)
+            {
+                var record = new CompilerData
+                {
+                    BinaryType = "PE",
+                    CompilerName = ".NET Compiler",
+                    Language = nameof(Language.MSIL),
+                    DebuggingFileName = pdb.GlobalScope?.Name,
+                    DebuggingFileGuid = pdb.GlobalScope?.Guid.ToString(),
+                    FileVersion = target.PE.FileVersion?.FileVersion,
+                    CompilerBackEndVersion = target.PE.LinkerVersion.ToString(),
+                    CompilerFrontEndVersion = target.PE.LinkerVersion.ToString(),
+                    AssemblyReferences = string.Join(';', target.PE.GetAssemblyReferenceStrings()),
+                };
+
+                if (!records.ContainsKey(record))
+                {
+                    records[record] = null;
+                }
+            }
+            else
+            {
+                foreach (DisposableEnumerableView<Symbol> omView in pdb.CreateObjectModuleIterator())
+                {
+                    Symbol om = omView.Value;
+                    ObjectModuleDetails omDetails = om.GetObjectModuleDetails();
+
+                    var record = new CompilerData
+                    {
+                        BinaryType = "PE",
+                        ModuleName = omDetails?.Name,
+                        ModuleLibrary = omDetails?.Library,
+                        Dialect = omDetails.GetDialect(out _),
+                        CompilerName = omDetails.CompilerName,
+                        CommandLine = omDetails.RawCommandLine,
+                        Language = omDetails.Language.ToString(),
+                        DebuggingFileName = pdb.GlobalScope?.Name,
+                        FileVersion = target.PE.FileVersion?.FileVersion,
+                        DebuggingFileGuid = pdb.GlobalScope?.Guid.ToString(),
+                        CompilerBackEndVersion = omDetails.CompilerBackEndVersion.ToString(),
+                        CompilerFrontEndVersion = omDetails.CompilerFrontEndVersion.ToString(),
+                    };
+
+                    if (!records.ContainsKey(record))
+                    {
+                        records[record] = omDetails;
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<CompilerData, ObjectModuleDetails> kv in records)
+            {
+                context.CompilerDataLogger.Write(context, kv.Key);
+            }
+        }
+    }
+}
