ed772c362d3742d4c97a94afcfcaed5e93d37653
Shaopeng Li <v-shaopengli@microsoft.com>
2022-07-20 19:56:38 -0700

    Fix test

--- BuildAndTest.sh
+++ BuildAndTest.sh
@@ -9,29 +9,6 @@ fi
 dotnet build src/BinSkimUnix.sln --configuration Release
 
 dotnet test src/Test.FunctionalTests.BinSkim.Driver/Test.FunctionalTests.BinSkim.Driver.csproj --no-build --configuration Release /p:Platform="x64"
-if [ $? != 0 ]
-then
-  >&2 echo "Test(s) failed in project: Test.FunctionalTests.BinSkim.Driver"
-  exit $?
-fi
-
 dotnet test src/Test.FunctionalTests.BinSkim.Rules/Test.FunctionalTests.BinSkim.Rules.csproj --no-build --configuration Release /p:Platform="x64"
-if [ $? != 0 ]
-then
-  >&2 echo "Test(s) failed in project: Test.FunctionalTests.BinSkim.Rules"
-  exit $?
-fi
-
 dotnet test src/Test.UnitTests.BinaryParsers/Test.UnitTests.BinaryParsers.csproj --no-build --configuration Release /p:Platform="x64"
-if [ $? != 0 ]
-then
-  >&2 echo "Test(s) failed in project: Test.UnitTests.BinaryParsers"
-  exit $?
-fi
-
 dotnet test src/Test.UnitTests.BinSkim.Rules/Test.UnitTests.BinSkim.Rules.csproj --no-build --configuration Release /p:Platform="x64"
-if [ $? != 0 ]
-then
-  >&2 echo "Test(s) failed in project: Test.UnitTests.BinSkim.Rules"
-  exit $?
-fi
