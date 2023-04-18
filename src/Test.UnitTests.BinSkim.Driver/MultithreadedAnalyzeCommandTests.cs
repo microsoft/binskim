963c9123b88711bc7c7c2c71f9ae38661263cff7
Suvam Mukherjee <sumukherjee@microsoft.com>
2023-04-17 20:47:57 -0400

    Adding sample tests

--- src/Test.UnitTests.BinSkim.Driver/MultithreadedAnalyzeCommandTests.cs
+++ src/Test.UnitTests.BinSkim.Driver/MultithreadedAnalyzeCommandTests.cs
@@ -2,11 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Globalization;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 using FluentAssertions;
 
 using Microsoft.CodeAnalysis.IL;
+
+using Microsoft.Coyote;
+using Microsoft.Coyote.Specifications;
+using Microsoft.Coyote.SystematicTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 
 using Xunit;
 
@@ -21,6 +30,27 @@ namespace Microsoft.CodeAnalysis.BinSkim.Rules
         {
             s_randomSeed = DateTime.UtcNow.TimeOfDay.TotalMilliseconds;
             s_random = new Random((int)s_randomSeed);
+        }
+
+        [Fact]
+        public async Task TestTask()
+        {
+            int value = 0;
+            Task task = Task.Run(() =>
+            {
+                value = 1;
+            });
+
+            Xunit.Assert.Equal(0, value);
+            await task;
+        }
+
+        [Fact]
+        public void CoyoteTestTask()
+        {
+            Configuration configuration = Configuration.Create().WithTestingIterations(100);
+            var engine = TestingEngine.Create(configuration, TestTask);
+            engine.Run();
         }
 
         [Fact]
