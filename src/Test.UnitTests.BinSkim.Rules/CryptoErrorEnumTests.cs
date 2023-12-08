8dc637e3b918d764f880abe7bf816bf6f005380e
Shaopeng <81775155+shaopeng-gh@users.noreply.github.com>
2023-12-08 13:12:12 -0800

    BUG: Fix missing error code to error description mappings for `CryptoError` (#969)
    * Update BuildCryptoErrorDescriptions()
    
    * Update error message
    
    * BUG: Fix `ERR998.ExceptionInAnalyze`: `InvalidOperationException: Unrecognized crypto HRESULT: 0x80096011` for check `BA2022.SignSecurely` when the signature is malformed, by adding missing error code to error description mappings.
    
    * Update texts
    
    * add InternalsVisibleToAttribute
    
    ---------
    
    Co-authored-by: Michael C. Fanning <michael.fanning@microsoft.com>

---
+++ src/Test.UnitTests.BinSkim.Rules/CryptoErrorEnumTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.IL.Rules
+{
+    public class CryptoErrorEnumTests
+    {
+        [Fact]
+        public void CryptoErrorEnumTest_NoMissingValues()
+        {
+            IEnumerable<CryptoError> enumValues = Enum.GetValues(typeof(CryptoError)).Cast<CryptoError>();
+            Dictionary<CryptoError, string> dictionaryValues = RulesExtensionMethods.BuildCryptoErrorDescriptions();
+            var missingValues = enumValues.Where(value => !dictionaryValues.ContainsKey(value)).ToList();
+            missingValues.Should().HaveCount(0, "BuildCryptoErrorDescriptions() should contain all cases from CryptoError enum.");
+        }
+    }
+}
