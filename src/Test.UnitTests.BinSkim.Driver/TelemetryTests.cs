528fccedccb3c71c45ff06b3a43f7f6f71bf1340
Shaopeng <81775155+shaopeng-gh@users.noreply.github.com>
2024-03-29 16:35:12 -0700

    `BA4002.ReportElfOrMachoCompilerData` enabled by default (#990)
    * BA4002.ReportElfOrMachoCompilerData enabled by default
    
    * Release note

--- src/Test.UnitTests.BinSkim.Driver/TelemetryTests.cs
+++ src/Test.UnitTests.BinSkim.Driver/TelemetryTests.cs
@@ -119,5 +119,54 @@ namespace Microsoft.CodeAnalysis.BinSkim.Rules
                     "Whether DisableTelemetry is explicitly set to false or not, the results should be the same.");
             }
         }
+
+        [Fact]
+        public void Telemetry_ReportElfOrMachoCompilerData_EnabledAndWorkingByDefault()
+        {
+            if (!PlatformSpecificHelpers.RunningOnWindows()) { return; }
+
+            using var assertionScope = new AssertionScope();
+
+            string sarifFile = Path.Combine(Path.GetTempPath(), "AnalyzeCommand_Telemetry_ReportElfOrMachoCompilerData_EnabledByDefault.sarif");
+            string csvFile = Path.Combine(Path.GetTempPath(), "AnalyzeCommand_Telemetry_ReportElfOrMachoCompilerData_EnabledByDefault.csv");
+            string configFile = Path.Combine(Path.GetTempPath(), "AnalyzeCommand_Telemetry_ReportElfOrMachoCompilerData_EnabledByDefault.xml");
+
+            File.Delete(sarifFile);
+            File.Delete(csvFile);
+            File.Delete(configFile);
+
+            string configFileContent = $"<?xml version=\"1.0\" encoding=\"utf-8\"?><Properties><Properties Key=\"CompilerTelemetry.Options\"><Property Key=\"CsvOutputPath\" Value=\"{csvFile}\"/></Properties></Properties>";
+            File.WriteAllText(configFile, configFileContent);
+
+            string pathToTestFile = Path.Combine(PEBinaryTests.TestData, "Dwarf", "hello-dwarf5-o2");
+            var options = new AnalyzeOptions
+            {
+                TargetFileSpecifiers = new string[] {
+                    pathToTestFile
+                },
+                OutputFilePath = sarifFile,
+                OutputFileOptions = new[] { FilePersistenceOptions.ForceOverwrite },
+                DataToInsert = new[] { OptionallyEmittedData.Hashes },
+                ConfigurationFilePath = configFile,
+            };
+
+            var command = new MultithreadedAnalyzeCommand();
+            command.Run(options);
+
+            bool fileExists = File.Exists(csvFile);
+            fileExists.Should().BeTrue("because the telemetry is enabled");
+
+            if (fileExists)
+            {
+                string fileContent = File.ReadAllText(csvFile);
+                fileContent.Should().Contain("-gdwarf-5", "because the rule ReportElfOrMachoCompilerData is enabled by default");
+            }
+
+            File.Delete(csvFile);
+            options.DisableTelemetry = true;
+            command.Run(options);
+            fileExists = File.Exists(csvFile);
+            fileExists.Should().BeFalse("because the telemetry is disabled");
+        }
     }
 }
