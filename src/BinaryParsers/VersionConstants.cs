bea11aeaeb3294abdc937ac1dfcf126f4365809a
Marek Aldorf <marekaldorf@microsoft.com>
2025-02-18 19:41:34 +0100

    Fix formatting in VersionConstants.cs for consistency

--- src/BinaryParsers/VersionConstants.cs
+++ src/BinaryParsers/VersionConstants.cs
@@ -9,4 +9,4 @@ namespace Microsoft.CodeAnalysis.IL
         public const string FileVersion = "4.3.1" + ".1";
         public const string Version = AssemblyVersion + Prerelease;
     }
-}                                                    
+}
