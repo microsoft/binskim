710035e3ae828c1e31b9298e6beedfa9597d730d
Steve Pfister <stpfiste@microsoft.com>
2025-02-18 16:37:02 -0500

    Formatting nit

--- src/BinaryParsers/PEBinary/PortableExecutable/PE.cs
+++ src/BinaryParsers/PEBinary/PortableExecutable/PE.cs
@@ -84,7 +84,7 @@ namespace Microsoft.CodeAnalysis.BinaryParsers.PortableExecutable
 
         public static bool IsNonWindowsMachineTarget(Machine machine)
         {
-            return machine != Machine.Amd64 && machine != Machine.I386 && machine != Machine.Arm && machine != Machine.Arm64 
+            return machine != Machine.Amd64 && machine != Machine.I386 && machine != Machine.Arm && machine != Machine.Arm64
                 && machine != Machine.IA64 && machine != Machine.Thumb;
         }
 
