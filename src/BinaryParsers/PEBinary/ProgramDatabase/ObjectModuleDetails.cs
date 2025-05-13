01f7d892cc8bc65c3e3d8d118001b882aa9d058c
Sára Rosecká <sraroseck@microsoft.com>
2025-05-13 23:06:40 +0200

    added Rust language

--- src/BinaryParsers/PEBinary/ProgramDatabase/ObjectModuleDetails.cs
+++ src/BinaryParsers/PEBinary/ProgramDatabase/ObjectModuleDetails.cs
@@ -202,6 +202,20 @@ namespace Microsoft.CodeAnalysis.BinaryParsers.ProgramDatabase
                     this.wellKnownCompiler = WellKnownCompilers.ClangLLVMRustc;
                 }
             }
+
+            if(this.Language == Language.Rust)
+            {
+                if(this.CompilerName == CompilerNames.ClangLLVMPrefix)
+                {
+                    this.wellKnownCompiler = WellKnownCompilers.ClangLLVMRustc;
+                }else if (CompilerName == CompilerNames.ClangLLVMRustcPrefix)
+                {
+                    this.wellKnownCompiler = WellKnownCompilers.ClangLLVMRustc;
+                }else if(CompilerName == CompilerNames.ClangPrefix)
+                {
+                    this.wellKnownCompiler = WellKnownCompilers.Clang;
+                }
+            }
         }
 
         public bool HasSecurityChecks { get; private set; }
