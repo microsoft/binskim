4983329db07ede1cc8ea1645b07ee81867420819
Nick Guerrera <nguerrera@microsoft.com>
2025-09-05 10:30:44 -0500

    Fix paths in BuildAndTest.sh

--- BuildAndTest.sh
+++ BuildAndTest.sh
@@ -2,7 +2,7 @@
 
 dotnet build src/BinSkim.sln --configuration Release
 
-dotnet test bld/bin/Release/net9.0/Test.FunctionalTests.BinSkim.Driver.dll
-dotnet test bld/bin/Release/net9.0/Test.FunctionalTests.BinSkim.Rules.dll
-dotnet test bld/bin/Release/net9.0/Test.UnitTests.BinaryParsers.dll
-dotnet test bld/bin/Release/net9.0/Test.UnitTests.BinSkim.Rules.dll
+dotnet test bld/bin/Test.FunctionalTests.BinSkim.Driver/release/Test.FunctionalTests.BinSkim.Driver.dll
+dotnet test bld/bin/Test.FunctionalTests.BinSkim.Rules/release/Test.FunctionalTests.BinSkim.Rules.dll
+dotnet test bld/bin/Test.UnitTests.BinaryParsers/release/Test.UnitTests.BinaryParsers.dll
+dotnet test bld/bin/Test.UnitTests.BinSkim.Rules/release/Test.UnitTests.BinSkim.Rules.dll
