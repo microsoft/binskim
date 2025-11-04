847ebbf7cf3892f672a25d7662781c1cdce55654
Michael C. Fanning <michael.fanning@microsoft.com>
2025-11-04 02:15:59 -0800

    Updated build and baselining (#1106)
    * Hand-off of work in progress on BinSkim build cleanup
    
    1. Stop Using Platforms/Platform and rely on RuntimeIdentifier only.
    2. Use ArtifactsPath to move output directories. Projects were building to the same directory and this is risky and not really supported.
    3. Replace hack to overwrite files in microsoft.diagnostics.tracing.tracevent with a hack to exclude the conflicting files in the build.
    4. Stop using local package directory that looks to have been just for (3).
    5. Disable CandidateAssemblyFile resolution, which was a long part of debugging the conflict with traceevent. This should be really have been off by default and it reminds to go disable it in our other repos.
    6. Don't set RuntimeIdentifiers in every project, it is only needed in the root exe project and putting it everywhere is triggering more project builds than necessary.
    7. Use TargetFramework instead of TargetFrameworks (plural) since there's only one TFM. Make build output folders tidier and speeds up the build.
    8. Remove unused x86 FCIBs and unused CommandLine.dll FCIB.
    9. Don't set SelfContained on exe project. Let the publish do this. Speeds up iteration time in IDE.
    10. Remove workaround in BuildAndTest.sh for bug fixed in 2017.
    11. Don't pass configuration to dotnet restore, it is a cross-configuration operation.
    TO DO:
    - Consolidate the many copies of GetTestDirectory into one place. It's painful to find and fix all of these when an output directory change is made.
    - Validate packages. I stopped when I got them to pack again.
    - Think about better resolution for conflict with traceevent.
    - I am not confident that any code path through traceevent will work though it was probably already broken by prior hack.
    - Does sarif-sdk need this as a hard dependency?
    - Can we consume the same Dia libs as them? I got compilation errors using the Dia2Lib that flows to us from their package and didn't investigate further.
    -
    
    * Remove unnecessary duplicate build code
    
    Also add comments and do x64 unofficial deployment only on x64.
    
    * Fix paths in BuildAndTest.sh
    
    * Keep net9.0 folder in nupkg to avoid breaking change
    
    * Restore sln header
    
    * Rebaseline files using converged logic.
    
    * Update all baselines.
    
    * Update baselines
    
    * Stabilize sorting
    
    * PR feedback
    
    * Updates from PR review
    
    * Move option in csproj
    
    * PR feedback.
    
    * Update baselines to remove MS org
    
    * Remove hashes arg as these can't be injected with relative path utilization
    
    * Final baseline update (?)
    
    * Test path forward slashes
    
    * Update baselines
    
    * Update baselines
    
    * Revert baselines again
    
    * Reformatting.
    
    * Update baselines
    
    * Revert bad merge of project file.
    
    ---------
    
    Co-authored-by: Nick Guerrera <nguerrera@microsoft.com>
    Co-authored-by: Martin Reznik <marez@microsoft.com>

---
+++ src/BinSkim.Driver/NormalizingSarifLogger.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.CodeAnalysis.Sarif.Writers;
+
+namespace Microsoft.CodeAnalysis.IL
+{
+    internal class NormalizingSarifLogger : IAnalysisLogger, IDisposable
+    {
+        private IAnalysisLogger _innerLogger;
+        private readonly SarifRewritingVisitor _pathRewritingVisitor;
+
+        public NormalizingSarifLogger(IAnalysisLogger innerLogger, string enlistmentRoot)
+        {
+            _innerLogger = innerLogger;
+
+            if (!string.IsNullOrEmpty(enlistmentRoot))
+            {
+                _pathRewritingVisitor = new PathRewritingVisitor(enlistmentRoot);
+            }
+
+            NormalizeRunTool(innerLogger);
+        }
+
+        private void NormalizeRunTool(IAnalysisLogger innerLogger)
+        {
+            const BindingFlags Flags = BindingFlags.Instance | BindingFlags.NonPublic;
+            Type t = typeof(SarifLogger);
+
+            // Try private/internal property first (newer SDKs), then known field names (older SDKs)
+            Run run = (Run)t.GetField("_run", Flags)?.GetValue((SarifLogger)innerLogger);
+
+            if (run?.Tool?.Driver != null)
+            {
+                run.Tool.Driver.Name = "BinSkim";
+                run.Tool.Driver.Organization = null;
+                run.Tool.Driver.Product = null;
+                run.Tool.Driver.FullName = null;
+                run.Tool.Driver.Version = default;
+                run.Tool.Driver.SemanticVersion = default;
+                run.Tool.Driver.DottedQuadFileVersion = default;
+
+                run.Tool.Driver.RemoveProperty("comments");
+            }
+        }
+
+        public FileRegionsCache FileRegionsCache { get => _innerLogger.FileRegionsCache; set => _innerLogger.FileRegionsCache = value; }
+
+        public void AnalysisStarted()
+        {
+            _innerLogger.AnalysisStarted();
+        }
+
+        public void AnalysisStopped(RuntimeConditions runtimeConditions)
+        {
+            _innerLogger.AnalysisStopped(runtimeConditions);
+        }
+
+        public void AnalyzingTarget(IAnalysisContext context)
+        {
+            _innerLogger.AnalyzingTarget(context);
+        }
+
+        public void Log(ReportingDescriptor rule, Result result, int? extensionIndex = null)
+        {
+            _pathRewritingVisitor?.Visit(rule);
+            _pathRewritingVisitor?.Visit(result);
+            _innerLogger.Log(rule, result, extensionIndex);
+        }
+
+        public void LogConfigurationNotification(Notification notification)
+        {
+            _pathRewritingVisitor?.Visit(notification);
+            _innerLogger.LogConfigurationNotification(notification);
+        }
+
+        public void LogToolNotification(Notification notification, ReportingDescriptor associatedRule = null)
+        {
+            _pathRewritingVisitor?.Visit(notification);
+            _innerLogger.LogToolNotification(notification, associatedRule);
+        }
+
+        public void TargetAnalyzed(IAnalysisContext context)
+        {
+            _innerLogger.TargetAnalyzed(context);
+        }
+
+        public void Dispose()
+        {
+            ((SarifLogger)_innerLogger)?.Dispose();
+            _innerLogger = null;
+        }
+
+        private class PathRewritingVisitor : SarifRewritingVisitor
+        {
+            private readonly string _enlistmentRoot;
+            public PathRewritingVisitor(string enlistmentRoot)
+            {
+                _enlistmentRoot = enlistmentRoot;
+            }
+            public override ArtifactLocation VisitArtifactLocation(ArtifactLocation artifactLocation)
+            {
+                artifactLocation = base.VisitArtifactLocation(artifactLocation);
+
+                if (artifactLocation?.Uri != null && !string.IsNullOrEmpty(_enlistmentRoot))
+                {
+                    string path = artifactLocation.Uri.GetFilePath();
+                    if (path.StartsWith(_enlistmentRoot, StringComparison.OrdinalIgnoreCase))
+                    {
+                        string relativePath = path.Substring(_enlistmentRoot.Length).TrimStart('\\', '/');
+                        relativePath = relativePath.Replace('\\', '/');
+                        artifactLocation.Uri = new Uri(relativePath, UriKind.Relative);
+                    }
+                }
+
+                return artifactLocation;
+            }
+        }
+    }
+}
