c0359db1083a0285749179411dc4094eb0f665c4
Sára Rosecká <sraroseck@microsoft.com>
2025-11-25 13:23:16 +0100

    Add support for detecting Rust compiler in ELF binaries
    Added `Rust` to the `ElfCompilerType` enum to classify binaries
    compiled with the Rust compiler. Updated the `compilerRegexes`
    list with a new regex pattern to identify Rust compiler strings.
    This extends support for recognizing Rust alongside GCC and Clang.

--- src/BinaryParsers/ElfBinary/ElfCompiler.cs
+++ src/BinaryParsers/ElfBinary/ElfCompiler.cs
@@ -12,7 +12,8 @@ namespace Microsoft.CodeAnalysis.BinaryParsers
     {
         Unknown = 0,
         Clang = 1,
-        GCC = 2
+        GCC = 2,
+        Rust = 3
     }
 
     /// <summary>
@@ -29,6 +30,7 @@ namespace Microsoft.CodeAnalysis.BinaryParsers
         {
             (new Regex(@"GCC:.+"), ElfCompilerType.GCC),
             (new Regex(@".*clang version.*"), ElfCompilerType.Clang),
+            (new Regex(@"rust .+"), ElfCompilerType.Rust), // Rust compiler, but we don't have a specific enum for it.
             (new Regex(@".*"), ElfCompilerType.Unknown)
         };
 
